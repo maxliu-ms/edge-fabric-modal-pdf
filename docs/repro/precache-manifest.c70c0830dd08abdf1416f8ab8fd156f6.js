012d2fab8413f06825b089ac8adaffc1bcd7b8a8
Max Liu <maxliu@microsoft.com>
2019-04-03 13:59:23 -0700

    repro

---
+++ docs/repro/precache-manifest.c70c0830dd08abdf1416f8ab8fd156f6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "64b12461744bd33a6d9b",
+    "url": "/static/css/main.2dc69896.chunk.css"
+  },
+  {
+    "revision": "64b12461744bd33a6d9b",
+    "url": "/static/js/main.e0570e51.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "e67e8f918d73321c056b",
+    "url": "/static/js/2.e5189208.chunk.js"
+  },
+  {
+    "revision": "93f2567f634e7172d3eab379c4cbbd82",
+    "url": "/index.html"
+  }
+];
